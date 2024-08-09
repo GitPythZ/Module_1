7720ee6dfeea13d5dad127253960c227c574c292
GitPythZ <yardbirds1957@gmail.com>
2024-08-10 00:18:28 +0300

    Строки

---
+++ homework4.py
@@ -0,0 +1,14 @@
+#
+my_string = input('Кто ты, Воин?:')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+print('Кто ты, Воин?:'.upper())
+print('Кто ты, Воин?:'.lower())
+print('Кто ты, Воин?:'.replace(' ', ''))
+print('Кто ты, Воин?:'[0])
+print('Кто ты, Воин?:'[-1])
