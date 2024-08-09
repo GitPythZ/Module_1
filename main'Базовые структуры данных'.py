c10132fed2e6f80a5402605dab6f4884f8081ecf
GitPythZ <yardbirds1957@gmail.com>
2024-08-10 00:16:14 +0300

    Вводные уроки

---
+++ main'Базовые структуры данных'.py
@@ -0,0 +1,34 @@
+#Старая задача
+# 'длина слова'
+print('Hello, world!')
+print(type('Hello, world!'))
+a='Hello, world!'
+print(a)
+print(len(a))
+#Задача:суммы и разности
+first = 5
+second = 7
+print(first + second)
+print(first-second)
+summa = 12
+diff = -2
+print(summa)
+print(diff)
+#Задача: Среднее арифметическое
+first = 5
+second = 9
+third = 10
+print((first+second+third)/3)
+mean=8
+print(mean)
+# Задача:Простые строчки
+first_string = 'Понедельник, '
+second_string = 'Вторник'
+print(first_string+second_string)
+# Задача: сложная формула
+a=10
+b=100
+c=1000
+print(a+b+c)
+f=(a*b)+(a*c)
+print(((f)**3)/2)
---
+++ main.py
@@ -0,0 +1,29 @@
+# '1st program'
+print(9**0.5*5)
+# '2nd program'
+print(9.99>9.98 and 1000 != 1000.1)
+# '3rd program' - 1234 and 5678
+print(int((1234%1000//10)+(5678%1000/10)))
+# '4th program' 13.42 and 42.13
+print(int(13.42) == int(42.13%1*100)) or int(42.13) == round(13.42%1*100)
+print(int(13.42) == int(42.13%1*100)) and int(42.13) == round(13.42%1*100)
+import this
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
